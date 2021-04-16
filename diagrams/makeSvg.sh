22a840cd0b0957bcd3da1506141fc26f49755103
Terry Brady <terrywbrady@gmail.com>
2021-04-16 09:30:44 -0700

    update make script

--- diagrams/makeSvg.sh
+++ diagrams/makeSvg.sh
@@ -2,13 +2,11 @@
 
 # Run this script from the repository root
 
-args=""
 for file in diagrams/*.mmd
 do
   if [ "$file" -nt "${file}.svg" ]
   then
-    args="${args} -i ${file/diagrams\//}"
+    echo "${file} changed"
+    docker run --rm -v "$(pwd)/diagrams:/data" minlag/mermaid-cli mmdc -w 1200 -i ${file/diagrams\//}
   fi
 done
-echo "Generate: ${args}"
-docker run --rm -v "$(pwd)/diagrams:/data" minlag/mermaid-cli mmdc -w 1200 $args
