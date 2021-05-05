4f5fdaa13f414f3d83f9c17f7d7132140bea1f2a
Terry Brady <terrywbrady@gmail.com>
2021-05-05 11:22:23 -0700

    update script

--- diagrams/makeSvg.sh
+++ diagrams/makeSvg.sh
@@ -10,6 +10,6 @@ do
   then
     echo "${file} changed"
     # docker run --rm -v "$(pwd)/diagrams:/data" minlag/mermaid-cli mmdc -w 1200 -i ${file/diagrams\//}
-    docker run --rm -v "$(pwd)/diagrams:/data" minlag/mermaid-cli mmdc -i ${file/diagrams\//}
+    docker run --rm -v "$(pwd)/diagrams:/data" -w /data minlag/mermaid-cli:8.9.2 mmdc -i ${file/diagrams\//}
   fi
 done
