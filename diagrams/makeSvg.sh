3d33380bad52f1918eda87ca3952455eef6092d0
Terry Brady <terrywbrady@gmail.com>
2021-11-17 16:28:48 -0800

    refine slides, update mermaid

--- diagrams/makeSvg.sh
+++ diagrams/makeSvg.sh
@@ -5,7 +5,7 @@
 #   https://github.com/mermaid-js/mermaid-cli/issues/114
 
 MERCLIVER=8.9.2
-MERCLIVER=8.9.3-1
+MERCLIVER=8.13.3
 
 for file in diagrams/*.mmd
 do
@@ -13,6 +13,6 @@ do
   then
     echo "${file} changed --> ${file/diagrams\//}"
     # docker run --rm -v "$(pwd)/diagrams:/data" minlag/mermaid-cli mmdc -w 1200 -i ${file/diagrams\//}
-    docker run --rm -v "$(pwd)/diagrams:/data" -w /data minlag/mermaid-cli:${MERCLIVER} mmdc -i ${file/diagrams\//}
+    docker run --rm -v "$(pwd)/diagrams:/data" minlag/mermaid-cli:${MERCLIVER} -i /data/${file/diagrams\//}
   fi
 done
